ef11fa55f86fc69af2484e24f1862a0800dbaee3
vit9696 <vit9696@users.noreply.github.com>
2022-04-04 01:14:12 +0300

    Hack for zeitwerk

--- app/overrides/documents/show.rb
+++ app/overrides/documents/show.rb
@@ -2,3 +2,6 @@ Deface::Override.new :virtual_path => 'documents/show',
   :name => 'add-move-link-to-documents-show',
   :insert_top => 'div.contextual',
   :partial => 'documents/show_move'
+
+module Documents::Show
+end
